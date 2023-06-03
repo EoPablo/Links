06686ee46c28fa00b69393fe8ef85795a346ce3b
Pablo <pablocavalcantee@gmail.com>
2023-06-03 12:50:18 -0300

    quiz add

---
+++ projetos/Quiz/questions.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    question: "Quanto é 1 + 2?",
+    answers: [
+      { option: "3", correct: true },
+      { option: "2", correct: false },
+      { option: "4", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 5 x 2?",
+    answers: [
+      { option: "9", correct: false },
+      { option: "10", correct: true },
+      { option: "11", correct: false },
+    ],
+  },
+  {
+    question: "Qual a capital do Brasil?",
+    answers: [
+      { option: "Buenos Aires", correct: false },
+      { option: "Madrid", correct: false },
+      { option: "Brasília", correct: true },
+    ],
+  },
+  {
+    question: "Quanto é 7 x 7?",
+    answers: [
+      { option: "70", correct: false },
+      { option: "56", correct: false },
+      { option: "49", correct: true },
+    ],
+  },
+  {
+    question: "Nome de quem desenvolveu o Quiz?",
+    answers: [
+      { option: "Augusto", correct: false },
+      { option: "Pedro", correct: false },
+      { option: "Pablo", correct: true },
+    ],
+  },
+]
